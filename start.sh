ad77a702461582804d6a4caea13a6cedbc457201
Micke Nordin <kano@sunet.se>
2025-04-04 08:51:25 +0200

    Go back to shib version: 1.6.9-apache-2

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+export KEYDIR=/etc/shibboleth/certs
+if [ ! -f "$KEYDIR/sp-cert.pem" ]; then
+	shib-keygen -o $KEYDIR -n sp
+fi
+
+mkfifo -m 600 /tmp/logpipe-shib
+chown _shibd:_shibd /tmp/logpipe-shib
+unbuffer cat /tmp/logpipe-shib &
+service shibd start
 echo -e "ServerName https://$(hostname -f):443\nUseCanonicalName On\n" >> /etc/apache2/apache2.conf
 cd /var/www/html && lessc --relative-urls -x plugins/libkolab/skins/elastic/libkolab.less > plugins/libkolab/skins/elastic/libkolab.min.css
 env APACHE_LOCK_DIR=/var/lock/apache2 APACHE_RUN_DIR=/var/run/apache2 APACHE_PID_FILE=/var/run/apache2/apache2.pid APACHE_RUN_USER=www-data APACHE_RUN_GROUP=www-data APACHE_LOG_DIR=/var/log/apache2  /docker-entrypoint.sh apache2 -DFOREGROUND
