95daa78d777f896d7ea8acc91f0efb38fc1019bc
Rikard Danielsson <richir@sunet.se>
2025-04-07 16:08:01 +0200

    adding full path to dependency for libkolab (before it is copied to /var/www/html)

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ chown _shibd:_shibd /tmp/logpipe-shib
 unbuffer cat /tmp/logpipe-shib &
 service shibd start
 echo -e "ServerName https://$(hostname -f):443\nUseCanonicalName On\n" >> /etc/apache2/apache2.conf
-lessc --rewrite-urls=all plugins/libkolab/skins/elastic/libkolab.less > plugins/libkolab/skins/elastic/libkolab.min.css
+lessc --rewrite-urls=all --include-path=/usr/src/roundcubemail/skins/elastic/styles/mixins.less plugins/libkolab/skins/elastic/libkolab.less > plugins/libkolab/skins/elastic/libkolab.min.css
 composer require --no-interaction \
     pear/http_request2 \
     caxy/php-htmldiff \
